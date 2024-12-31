633acc89ba1bb272ae1879059f57bccc459e6d78
Sam Jacob <91039661+sam21nw@users.noreply.github.com>
2024-12-31 05:56:41 +0100

    yargs

--- notes-app/main.ts
+++ notes-app/main.ts
@@ -1,1 +1,33 @@
-console.log(process.argv);
+import yargs from 'yargs';
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        console.log('Title:', argv.title);
+        console.log('Body:', argv.body);
+    }
+})
+    .help()
+    .argv;
+
+
+const command: string = process.argv[2];
+
+if (command == "add") {
+    console.log("Adding note!");
+} else if (command == "remove") {
+    console.log("Removing note!");
+}
